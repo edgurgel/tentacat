819e57be69c628e59dfa01384ba5451e626ca8d7
Iuri Fernandes <iurifq@gmail.com>
2013-06-23 23:03:24 -0300

    Add following function to followers module

--- lib/tentacat/followers.ex
+++ lib/tentacat/followers.ex
@@ -6,6 +6,20 @@ defmodule Tentacat.Users.Followers do
 
   ## Example
 
+      Tentacat.Users.Followers.following(access_token: "91898192219")
+
+  More info at: http://developer.github.com/v3/users/followers/#list-users-followed-by-another-user
+  """
+  @spec following(Client.auth) :: :jsx.json_term
+  def following(auth) do
+    get "user/following", auth
+  end
+
+  @doc """
+  List users the authenticated user follows
+
+  ## Example
+
       Tentacat.Users.Followers.followers(access_token: "91898192219")
 
   More info at: http://developer.github.com/v3/users/followers/#list-followers-of-a-user
