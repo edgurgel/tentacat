1e85a9656b073279b13d5974b9921c7ecd45f206
Eduardo Gurgel <eduardo@gurgel.me>
2014-05-09 19:42:55 +1200

    Update deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Tentacat.Mixfile do
 
   defp deps do
    [ { :httpoison, github: "edgurgel/httpoison", tag: "0.1.0" },
-     { :jsex, github: "talentdeficit/jsex" },
-     { :meck, github: "eproxus/meck", ref: "69f02255a8219185bf55da303981d86886b3c24b", only: :test } ]
+     { :jsex, "~> 2.0" },
+     { :meck, github: "eproxus/meck", tag: "0.8.2", only: :test } ]
   end
 end
