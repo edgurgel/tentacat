451d72b603d6663b853156f95cdf67417bd3997c
Jamie Winsor <jamie@vialstudios.com>
2014-05-30 13:16:24 -0700

    remove lock on httpoison 0.1.0 tag

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Tentacat.Mixfile do
   end
 
   defp deps do
-   [ { :httpoison, github: "edgurgel/httpoison", tag: "0.1.0" },
+   [ { :httpoison, github: "edgurgel/httpoison" },
      { :jsex, "~> 2.0" },
      { :meck, github: "eproxus/meck", tag: "0.8.2", only: :test } ]
   end
