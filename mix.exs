c3946d2b90bac31adf162734dcef2cda13e21592
Eduardo Gurgel <eduardo@gurgel.me>
2014-08-17 22:46:37 +1200

    Bump version 0.1.0
    Drop support to 0.14.*

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Tentacat.Mixfile do
 
   def project do
     [ app: :tentacat,
-      version: "0.0.1",
-      elixir: "~> 0.14.3 or ~> 0.15.0",
+      version: "0.1.0",
+      elixir: "~> 0.15.0",
       deps: deps ]
   end
 
