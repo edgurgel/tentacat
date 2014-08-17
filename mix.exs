6673e8aff2131075badb176cb6e1a785bb5884fc
Eduardo Gurgel <eduardo@gurgel.me>
2014-08-17 22:50:34 +1200

    Add package description to publish to hex

--- mix.exs
+++ mix.exs
@@ -1,10 +1,17 @@
 defmodule Tentacat.Mixfile do
   use Mix.Project
 
+  @description """
+    Simple Elixir wrapper for the GitHub API
+  """
+
   def project do
     [ app: :tentacat,
       version: "0.1.0",
       elixir: "~> 0.15.0",
+      name: "Tentacat",
+      description: @description,
+      package: package,
       deps: deps ]
   end
 
@@ -16,5 +23,11 @@ defmodule Tentacat.Mixfile do
    [ { :httpoison, "~> 0.4.0" },
      { :jsex, "~> 2.0" },
      { :meck, "~> 0.8.2", only: :test } ]
+  end
+
+  defp package do
+    [ contributors: ["Eduardo Gurgel Pinho", "Jamie Winsor"],
+      licenses: ["MIT"],
+      links: [ { "Github", "https://github.com/edgurgel/tentacat" } ] ]
   end
 end
