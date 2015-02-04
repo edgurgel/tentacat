386e06968822a9320868b3a7c025fc13c2ca9325
Grigory Starinkin <starinkin@gmail.com>
2015-02-04 12:26:02 +0300

    Added PR basics, referencies, branches, trees

---
+++ lib/tentacat/contents.ex
@@ -0,0 +1,113 @@
+defmodule Tentacat.Contents do
+  import Tentacat
+  alias Tentacat.Client
+
+  @doc """
+  Get the contents of a file or directory in a repository.
+
+  ## Example
+
+      Tentacat.Contents.find "elixir-lang", "elixir", "lib"
+      Tentacat.Contents.find "elixir-lang", "elixir", "lib", client
+
+  More info at: https://developer.github.com/v3/repos/contents/#get-contents
+  """
+  @spec find(binary, binary, binary, Client.t) :: Tentacat.response
+  def find(owner, repo, path, client \\ %Client{}) do
+    get "repos/#{owner}/#{repo}/contents/#{path}", client.auth
+  end
+
+  @doc """
+  Get the contents of a file or directory in a repository for a provided reference
+
+  ## Example
+
+      Tentacat.Contents.find_in "elixir-lang", "elixir", "lib", "ref-name"
+      Tentacat.Contents.find_in "elixir-lang", "elixir", "lib", "ref-name", client
+
+  More info at: https://developer.github.com/v3/repos/contents/#get-contents
+  """
+  @spec find_in(binary, binary, binary, binary, Client.t) :: Tentacat.response
+  def find_in(owner, repo, path, ref, client \\ %Client{}) do
+    get "repos/#{owner}/#{repo}/contents/#{path}", client.auth, [{:ref, ref}]
+  end
+
+  @doc """
+  Create a file
+
+  Create body example:
+
+      %{
+        "message" => "my commit message",
+        "committer" => %{
+          "name"  =>  "Grigory Starinkin",
+          "email" =>  "starinkin@gmail.com"
+        },
+        "content" => "bXkgbmV3IGZpbGUgY29udGVudHM=",
+        "branch"  => "cool-feature"
+      }
+
+  ## Example
+
+      Tentacat.Contents.create "elixir-lang", "elixir", "lib/elixir.ex", body, client
+
+  More info at: https://developer.github.com/v3/repos/contents/#create-a-file
+  """
+  @spec create(binary, binary, binary, list | map, Client.t) :: Tentacat.response
+  def create(owner, repo, path, body, client) do
+    put "repos/#{owner}/#{repo}/contents/#{path}", client.auth, body
+  end
+
+  @doc """
+  Update a file
+
+  Update body example:
+
+      %{
+        "message" => "my commit message",
+        "committer" => %{
+          "name"  => "Grigory Starinkin",
+          "email" => "starinkin@gmail.com"
+        },
+        "content" => "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz",
+        "sha"     => "329688480d39049927147c162b9d2deaf885005f",
+        "branch"  => "cool-feature"
+      }
+
+  ## Example
+
+      Tentacat.Contents.update "elixir-lang", "elixir", "lib/elixir.ex", body, client
+
+  More info at: https://developer.github.com/v3/repos/contents/#update-a-file
+  """
+  @spec update(binary, binary, binary, list | map, Client.t) :: Tentacat.response
+  def update(owner, repo, path, body, client) do
+    put "repos/#{owner}/#{repo}/contents/#{path}", client.auth, body
+  end
+
+  @doc """
+  Deletes a file in a repository
+
+  Delete body example:
+
+      %{
+        "message" => "my commit message",
+        "committer" => %{
+          "name"  => "Grigory Starinkin",
+          "email" => "starinkin@gmail.com"
+        },
+        "sha"    => "329688480d39049927147c162b9d2deaf885005f",
+        "branch" => "cool-feature"
+      }
+
+  ## Example
+
+      Tentacat.Contents.remove "elixir-lang", "elixir", "lib/elixir.ex", body, client
+
+  More info at: https://developer.github.com/v3/repos/contents/#delete-a-file
+  """
+  @spec remove(binary, binary, binary, list | map, Client.t) :: Tentacat.response
+  def remove(owner, repo, path, body, client) do
+    delete "repos/#{owner}/#{repo}/contents/#{path}", client.auth, body
+  end
+end
---
+++ lib/tentacat/trees.ex
@@ -0,0 +1,62 @@
+defmodule Tentacat.Trees do
+  import Tentacat
+  alias Tentacat.Client
+
+  @doc """
+  Get a Tree
+
+  ## Example
+
+      Tentacat.Trees.find "elixir-lang", "elixir", "eac64175b71fc598fa0b6e611dea6a58973170e9"
+      Tentacat.Trees.find "elixir-lang", "elixir", "eac64175b71fc598fa0b6e611dea6a58973170e9", client
+
+  More info at: https://developer.github.com/v3/git/trees/#get-a-tree
+  """
+  @spec find(binary, binary, binary, Client.t) :: Tentacat.response
+  def find(owner, repo, sha, client \\ %Client{}) do
+    get "repos/#{owner}/#{repo}/git/trees/#{sha}", client.auth
+  end
+
+  @doc """
+  Get a Tree Recursively
+
+  ## Example
+
+      Tentacat.Trees.find_recursive "elixir-lang", "elixir", "eac64175b71fc598fa0b6e611dea6a58973170e9"
+      Tentacat.Trees.find_recursive "elixir-lang", "elixir", "eac64175b71fc598fa0b6e611dea6a58973170e9", client
+
+  More info at: https://developer.github.com/v3/git/trees/#get-a-tree-recursively
+  """
+  @spec find_recursive(binary, binary, binary, Client.t) :: Tentacat.response
+  def find_recursive(owner, repo, sha, client \\ %Client{}) do
+    get "repos/#{owner}/#{repo}/git/trees/#{sha}", client.auth, [{:recursive, 1}]
+  end
+
+  @doc """
+  Create a Tree
+
+  Tree body example:
+
+      %{
+        "base_tree" => "9fb037999f264ba9a7fc6274d15fa3ae2ab98312",
+        "tree" => [
+          %{
+            "path" => "elixir.ex",
+            "mode" => "100644",
+            "type" => "blob",
+            "sha"  => "44b4fc6d56897b048c772eb4087f854f46256132"
+          }
+        ]
+      }
+
+  ## Example
+
+      Tentacat.Trees.create "elixir-lang", "elixir", body, client
+
+  More info at: https://developer.github.com/v3/git/trees/#create-a-tree
+  """
+  @spec create(binary, binary, list | map, Client.t) :: Tentacat.response
+  def create(owner, repo, body, client) do
+    post "repos/#{owner}/#{repo}/git/trees", client.auth, body
+  end
+end
