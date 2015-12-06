6af3b29a9e146e82500c04f38627b8ad3c1de01d
Hugo Duksis <duksis@gmail.com>
2015-12-06 22:13:12 +0100

    Moves Tentacat.Client to a separate file

--- lib/tentacat.ex
+++ lib/tentacat.ex
@@ -1,28 +1,6 @@
 defmodule Tentacat do
   use HTTPoison.Base
-
-  defmodule Client do
-    defstruct auth: nil, endpoint: "https://api.github.com/"
-
-    @type auth :: %{user: binary, password: binary} | %{access_token: binary}
-    @type t :: %__MODULE__{auth: auth, endpoint: binary}
-
-    @spec new() :: t
-    def new(), do: %__MODULE__{}
-
-    @spec new(auth) :: t
-    def new(auth),  do: %__MODULE__{auth: auth}
-
-    @spec new(auth, binary) :: t
-    def new(auth, endpoint) do
-      endpoint = if String.ends_with?(endpoint, "/") do
-        endpoint
-      else
-        endpoint <> "/"
-      end
-      %__MODULE__{auth: auth, endpoint: endpoint}
-    end
-  end
+  alias Tentacat.Client
 
   @user_agent [{"User-agent", "tentacat"}]
 
---
+++ lib/tentacat/client.ex
@@ -0,0 +1,22 @@
+defmodule Tentacat.Client do
+  defstruct auth: nil, endpoint: "https://api.github.com/"
+
+  @type auth :: %{user: binary, password: binary} | %{access_token: binary}
+  @type t :: %__MODULE__{auth: auth, endpoint: binary}
+
+  @spec new() :: t
+  def new(), do: %__MODULE__{}
+
+  @spec new(auth) :: t
+  def new(auth),  do: %__MODULE__{auth: auth}
+
+  @spec new(auth, binary) :: t
+  def new(auth, endpoint) do
+    endpoint = if String.ends_with?(endpoint, "/") do
+      endpoint
+    else
+      endpoint <> "/"
+    end
+    %__MODULE__{auth: auth, endpoint: endpoint}
+  end
+end
