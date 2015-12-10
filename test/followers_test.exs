845a1e09c244112bce4936f17462f1582bc7e37b
Hugo Duksis <duksis@gmail.com>
2015-12-10 23:30:07 +0100

    Adds tests for followers

---
+++ test/followers_test.exs
@@ -0,0 +1,63 @@
+defmodule Tentacat.FollowersTest do
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  import Tentacat.Users.Followers
+
+  doctest Tentacat.Users.Followers
+
+  @client Tentacat.Client.new(%{access_token: "8e663c8614ced27c09b963f806ac46776a29db50"})
+
+  setup_all do
+    HTTPoison.start
+  end
+
+  test "following/1" do
+    use_cassette "followers#following/1" do
+      assert following(@client) == []
+    end
+  end
+
+  test "following/2" do
+    use_cassette "followers#following/2" do
+      assert following("duksis", @client) == []
+    end
+  end
+
+  test "followers/1" do
+    use_cassette "followers#followers/1" do
+      [%{"login" => username}] = followers(@client)
+      assert username == "duksis"
+    end
+  end
+
+  test "followers/2" do
+    use_cassette "followers#followers/2" do
+      assert followers("duksis", @client) == []
+    end
+  end
+
+  test "following?/2" do
+    use_cassette "followers#following?/2" do
+      assert following?("duksis", @client) == true
+    end
+  end
+
+  test "following?/3" do
+    use_cassette "followers#following?/3" do
+      assert following?("torvalds", "duksis", @client) == false
+    end
+  end
+
+  test "follow/2" do
+    use_cassette "followers#follow" do
+      assert follow("duksis", @client) == true
+    end
+  end
+
+  test "unfollow/2" do
+    use_cassette "followers#unfollow" do
+      assert unfollow("duksis", @client) == true
+    end
+  end
+
+end
