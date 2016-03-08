fcf74ddc8237487b42a2fdb0d3a672f3008abc51
Hugo Duksis <duksis@gmail.com>
2016-03-08 22:09:30 +0100

    Update coveralls version

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Tentacat.Mixfile do
      {:earmark, "~> 0.2.1", only: :docs},
      {:ex_doc, "~> 0.11.4", only: :docs},
      {:inch_ex, only: :docs},
-     {:excoveralls, "~> 0.4", only: :test},
+     {:excoveralls, "~> 0.5", only: :test},
      {:exvcr, "~> 0.6", only: :test},
      { :meck, "~> 0.8", only: :test } ]
   end
