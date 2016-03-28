50bcd07eb266d35e591c1d8f23362bfa6d04919f
Jeff Weiss <jeff.a.weiss@gmail.com>
2016-03-27 23:28:47 -0700

    add groundwork for pagination: merging existing uri params with new ones

--- lib/tentacat.ex
+++ lib/tentacat.ex
@@ -54,13 +54,56 @@ defmodule Tentacat do
     endpoint <> path
   end
 
-  defp add_params_to_url(url, params) do
-    <<url :: binary, build_qs(params) :: binary>>
+  @doc """
+  Take an existing URI and add addition params, appending and replacing as necessary
+
+  ## Examples
+      iex> add_params_to_url("http://example.com/wat", [])
+      "http://example.com/wat"
+
+      iex> add_params_to_url("http://example.com/wat", [q: 1])
+      "http://example.com/wat?q=1"
+
+      iex> add_params_to_url("http://example.com/wat", [q: 1, t: 2])
+      "http://example.com/wat?q=1&t=2"
+
+      iex> add_params_to_url("http://example.com/wat?q=1&t=2", [])
+      "http://example.com/wat?q=1&t=2"
+
+      iex> add_params_to_url("http://example.com/wat?q=1", [t: 2])
+      "http://example.com/wat?q=1&t=2"
+
+      iex> add_params_to_url("http://example.com/wat?q=1", [q: 3, t: 2])
+      "http://example.com/wat?q=3&t=2"
+
+      iex> add_params_to_url("http://example.com/wat?q=1&s=4", [q: 3, t: 2])
+      "http://example.com/wat?q=3&s=4&t=2"
+  """
+  @spec add_params_to_url(binary, list) :: binary
+  def add_params_to_url(url, params) do
+    url
+    |> URI.parse
+    |> merge_uri_params(params)
+    |> URI.to_string
   end
 
-  @spec build_qs([{atom, binary}]) :: binary
-  defp build_qs([]), do: ""
-  defp build_qs(kvs), do: to_string('?' ++ URI.encode_query(kvs))
+  @spec merge_uri_params(URI.t, list) :: URI.t
+  defp merge_uri_params(uri, []), do: uri
+  defp merge_uri_params(%URI{query: nil} = uri, params) do
+    uri
+    |> Map.put(:query, URI.encode_query(params))
+  end
+  defp merge_uri_params(%URI{} = uri, params) do
+    uri
+    |> Map.update!(:query, fn q -> q |> URI.decode_query |> Map.merge(param_list_to_map_with_string_keys(params)) |> URI.encode_query end)
+  end
+
+  @spec param_list_to_map_with_string_keys(list) :: map
+  defp param_list_to_map_with_string_keys(list) do
+    for {key, value} <- list, into: Map.new do
+      {"#{key}", value}
+    end
+  end
 
   @doc """
   There are two ways to authenticate through GitHub API v3:
