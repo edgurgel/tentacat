09da86baacd470f921a3bbce8ad0b27c8344448c
Eduardo Gurgel <eduardo@gurgel.me>
2017-02-12 22:38:56 +1300

    Move docs dependencies to :dev environment

--- mix.exs
+++ mix.exs
@@ -24,9 +24,9 @@ defmodule Tentacat.Mixfile do
   defp deps do
    [ {:httpoison, "~> 0.8"},
      {:exjsx, "~> 3.2"},
-     {:earmark, "~> 0.2.1", only: :docs},
-     {:ex_doc, "~> 0.11.4", only: :docs},
-     {:inch_ex, "~> 0.5", only: :docs},
+     {:earmark, "~> 0.2.1", only: :dev},
+     {:ex_doc, "~> 0.11.4", only: :dev},
+     {:inch_ex, "~> 0.5", only: :dev},
      {:excoveralls, "~> 0.5", only: :test},
      {:exvcr, "~> 0.6", only: :test},
      {:meck, "~> 0.8", only: :test} ]
