58418a847ee5fd3e4a80422e7cc85f8facbbe759
Evelyn Masso <evelyn@carbonfive.com>
2017-11-20 11:44:40 -0800

    more github installations api coverage
    fix installation/app user methods

--- lib/tentacat/app/installations.ex
+++ lib/tentacat/app/installations.ex
@@ -17,6 +17,20 @@ defmodule Tentacat.App.Installations do
   end
 
   @doc """
+  List installations accessible with the authenticated app user
+
+  ## Example
+
+      Tentacat.App.Installations.list_for_user client
+
+  More info at: https://developer.github.com/v3/apps/#list-installations-for-user
+  """
+  @spec list_for_user(Client.t) :: Tentacat.response
+  def list_for_user(client) do
+    get "user/installations", client
+  end
+
+  @doc """
   Get a specific installation
 
   ## Example
@@ -42,6 +56,34 @@ defmodule Tentacat.App.Installations do
   @spec token(integer, Client.t) :: Tentacat.response
   def token(installation_id, client) do
     post "app/installations/#{installation_id}/access_tokens", client
+  end
+
+  @doc """
+  List repositories for the authenticated installation
+
+  ## Example
+
+      Tentacat.App.Installations.list_repositories client
+
+  More info at: https://developer.github.com/v3/apps/installations/#list-repositories
+  """
+  @spec list_repositories(Client.t) :: Tentacat.response
+  def list_repositories(client) do
+    get "installation/repositories", client
+  end
+
+  @doc """
+  List repositories in an installation that are accessible with to the authenticated app user
+
+  ## Example
+
+      Tentacat.App.Installations.list_repositories_for_user 154, client
+
+  More info at: https://developer.github.com/v3/apps/#list-installations-for-user
+  """
+  @spec list_repositories_for_user(integer, Client.t) :: Tentacat.response
+  def list_repositories_for_user(installation_id, client) do
+    get "user/installations/#{installation_id}/repositories", client
   end
 
 end
