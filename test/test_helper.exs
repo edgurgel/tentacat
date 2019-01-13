f683dd026de748fafc4981fbc2af6c2b88aebdd6
Sandis <m@sandis.lv>
2019-01-13 15:14:58 +0200

    Move VCR config, update HTTPoison to 1.0.0

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,2 @@
 ExUnit.configure(exclude: [skip: true])
 ExUnit.start()
-ExVCR.Config.cassette_library_dir("test/fixture/vcr_cassettes")
-ExVCR.Config.filter_sensitive_data("token [^\"]+", "token yourtokencomeshere")
