f7386132773fa908b0111f8e0d553810193be9d0
Arthur Chiu <hallo@achiurizo.co>
2019-04-24 16:27:52 -0700

    add Organizations.Projects#list/2
    adds GET /orgs/:organization/projects

---
+++ lib/tentacat/organizations/projects.ex
@@ -0,0 +1,18 @@
+defmodule Tentacat.Organizations.Projects do
+  import Tentacat
+  alias Tentacat.Client
+
+  @doc """
+  Lists the projects in a repository
+
+  ## Example
+
+      Tentacat.Organizations.Projects.list client, "elixir"
+
+  More info at: https://developer.github.com/v3/projects/#list-organization-projects
+  """
+  @spec list(Client.t(), binary) :: Tentacat.response()
+  def list(client, organization) do
+    get("orgs/#{organization}/projects", client)
+  end
+end
