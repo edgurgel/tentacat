fbd8f4a04eae52c094695c3295a06a4b3317c25e
Arthur Chiu <hallo@achiurizo.co>
2019-04-24 17:32:13 -0700

    add Repositories.Projects#create/4
    add POST /repos/:owner/:repo/projects

--- lib/tentacat/repositories/projects.ex
+++ lib/tentacat/repositories/projects.ex
@@ -15,4 +15,23 @@ defmodule Tentacat.Repositories.Projects do
   def list(client, owner, repo) do
     get("repos/#{owner}/#{repo}/projects", client)
   end
+
+  @doc """
+  Create a new project for a repository
+
+  Possible values for options:
+
+  * [name: "name of project board"]
+  * [body: "description of the project"]
+
+  ## Example
+
+    Tentacat.Repositories.Projects.create client, "elixir-lang", "elixir", name: "tentacat", body: "project board for tentacat"
+
+  More info at: https://developer.github.com/v3/projects/#create-a-repository-project
+  """
+  @spec create(Client.t(), binary, binary, list) :: Tentacat.response()
+  def create(client, owner, repo, options) do
+    post("repos/#{owner}/#{repo}/projects", client, options)
+  end
 end
