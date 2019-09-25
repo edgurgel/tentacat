7f254ff3df23d098a7e5f8fd714b394fa9223e1b
Mark <birmanmark@gmail.com>
2019-09-25 19:59:06 +0300

    Add a bunch of Gist getters

--- lib/tentacat/gists.ex
+++ lib/tentacat/gists.ex
@@ -3,6 +3,129 @@ defmodule Tentacat.Gists do
   alias Tentacat.Client
 
   @doc """
+  List current user's gists.
+
+  ## Examples
+
+      Tentacat.Gists.list_mine(client)
+
+  More info at: https://developer.github.com/v3/gists/#list-a-users-gists
+  """
+  @spec list_mine(Client.t(), Keyword.t()) :: Tentacat.response()
+  def list_mine(client, params \\ [], options \\ []) do
+    get("gists", client, params, options)
+  end
+
+  @doc """
+  List users gists.
+
+  ## Example
+
+      Tentacat.Gists.list_users(client, "steve")
+
+  More info at: https://developer.github.com/v3/gists/#list-a-users-gists
+  """
+  @spec list_users(Client.t(), binary, Keyword.t(), Keyword.t()) :: Tentacat.response()
+  def list_users(client \\ %Client{}, owner, params \\ [], options \\ []) do
+    get("users/#{owner}/gists", client, params, options)
+  end
+
+  @doc """
+  List all public gists
+
+  ## Example
+
+      Tentacat.Gists.list_public
+      Tentacat.Gists.list_public(client)
+
+  More info at: https://developer.github.com/v3/gists/#list-all-public-gists
+  """
+  @spec list_public(Client.t(), Keyword.t(), Keyword.t()) :: Tentacat.response()
+  def list_public(client \\ %Client{}, params \\ [], options \\ []) do
+    get("gists/public", client, params, Keyword.merge([pagination: :none], options))
+  end
+
+  @doc """
+  List starred gists
+
+  ## Example
+
+      Tentacat.Gists.list_starred
+      Tentacat.Gists.list_starred(client)
+
+  More info at: https://developer.github.com/v3/gists/#list-starred-gists
+  """
+  @spec list_starred(Client.t(), Keyword.t(), Keyword.t()) :: Tentacat.response()
+  def list_starred(client, params \\ [], options \\ []) do
+    get("gists/starred", client, params, Keyword.merge([pagination: :none], options))
+  end
+
+  @doc """
+  Get
+
+  ## Example
+
+      Tentacat.Gists.gist_get("fe771b85eeeff878d177b0c0f3840afd")
+      Tentacat.Gists.gist_get(client, "fe771b85eeeff878d177b0c0f3840afd")
+
+  More info at: https://developer.github.com/v3/gists/#get
+  """
+  @spec gist_get(Client.t(), binary) :: Tentacat.response()
+  def gist_get(client \\ %Client{}, gist_id) do
+    get("gists/#{gist_id}", client)
+  end
+
+  @doc """
+  Get a specific revision of a gist
+
+  ## Example
+
+      Tentacat.Gists.gist_get("fe771b85eeeff878d177b0c0f3840afd")
+      Tentacat.Gists.gist_get(client, "fe771b85eeeff878d177b0c0f3840afd")
+
+  More info at: https://developer.github.com/v3/gists/#get-a-specific-revision-of-a-gist
+  """
+  @spec get_revision(Client.t(), binary, binary) :: Tentacat.response()
+  def get_revision(client \\ %Client{}, gist_id, sha) do
+    get("gists/#{gist_id}/#{sha}", client)
+  end
+
+  @doc """
+  Create a new gist for the authenticated user.
+
+  Possible values for `options`:
+
+  * [files: "Simple Elixir wrapper for the GitHub API"]
+  * [description: "http://www.github.com/edgurgel/tentacat"]
+  * [public: false]
+
+  ## Example
+
+      Tentacat.Gists.create(client, "tentacat", private: false)
+
+  More info at: https://developer.github.com/v3/gists/#create-a-gist
+  """
+  @spec create(Client.t(), binary, list) :: Tentacat.response()
+  def create(client, gist, options \\ []) do
+    post("gists", client, List.flatten([name: gist], options))
+  end
+
+  @doc """
+  List forks
+
+  ## Example
+
+      Tentacat.Gists.list_forks("fe771b85eeeff878d177b0c0f3840afd")
+      Tentacat.Gists.list_forks(client, "fe771b85eeeff878d177b0c0f3840afd")
+
+  More info at: https://developer.github.com/v3/gists/#list-gist-forks
+  """
+  @spec list_forks(Client.t(), binary, Keyword.t()) :: Tentacat.response()
+  def list_forks(client \\ %Client{}, gist_id, params \\ []) do
+    get("gists/#{gist_id}/forks", client, params)
+  end
+
+  @doc """
   Deleting a gist requires admin access. If OAuth is used, the gist scope is required.
 
   ## Example
--- test/gists_test.exs
+++ test/gists_test.exs
@@ -11,6 +11,64 @@ defmodule Tentacat.GistsTest do
     HTTPoison.start()
   end
 
+  test "list_mine/2" do
+    use_cassette "gists#list_mine" do
+      assert length(elem(list_mine(@client), 1)) == 1
+    end
+  end
+
+  test "list_users/2" do
+    use_cassette "gists#list_users" do
+      {_, [%{"description" => description}], _} = list_users(@client, "octocat")
+      assert description == "Hello World Examples"
+    end
+  end
+
+  test "list_public/0" do
+    use_cassette "gists#list_public", match_requests_on: [:query] do
+      {_, [%{"public" => public}], _} = list_public(@client)
+      assert public
+    end
+  end
+
+  test "list_public/0 with parameters" do
+    use_cassette "gists#list_public_with_params", match_requests_on: [:query] do
+      {_, [%{"public" => public}], _} = list_public(@client, since: "2019-07-04 12:34")
+      assert public
+    end
+  end
+
+  test "list_starred/1" do
+    use_cassette "gists#list_starred" do
+      assert length(elem(list_starred(@client), 1)) == 1
+    end
+  end
+
+  test "gist_get/2" do
+    use_cassette "gists#gist_get" do
+      {status_code, _, _} = gist_get(@client, "fe771b85eeeff878d177b0c0f3840afd")
+      assert status_code == 200
+    end
+  end
+
+  test "get_revision/2" do
+    use_cassette "gists#get_revision" do
+      gist_id = "fe771b85eeeff878d177b0c0f3840afd"
+      revision_sha = "0ba06a873509677ab40b8ed5575f249a55c6fc41"
+      {status_code, %{"url" => url}, _} = get_revision(gist_id, revision_sha)
+      assert url == "https://api.github.com/gists/#{gist_id}/#{revision_sha}"
+      assert status_code == 200
+    end
+  end
+
+  test "list_forks/2" do
+    use_cassette "gists#list_forks" do
+      {status, response, _} = list_forks(@client, "0feff8bfe771b85ee0c3840afd78d177")
+      assert status == 200
+      assert length(response) == 1
+    end
+  end
+
   test "delete/2" do
     use_cassette "gists#delete" do
       {status, _response, _} = delete(@client, "0feff8bfe771b85ee0c3840afd78d177")
