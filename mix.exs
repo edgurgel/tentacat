25a28ede581ad6094d7d2a77919156feb6573c59
Eduardo Gurgel <eduardo@gurgel.me>
2020-05-22 10:17:21 +1200

    Bump version 2.0.0

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Tentacat.Mixfile do
   def project do
     [
       app: :tentacat,
-      version: "1.6.1",
+      version: "2.0.0",
       elixir: "~> 1.9",
       name: "Tentacat",
       description: @description,
