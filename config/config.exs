4a34d9d92f0376b3fef28482750115c44f5bd10c
Eduardo Gurgel <eduardo@gurgel.me>
2020-06-06 21:56:17 +1200

    Fix exvcr configuration

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
-config :exvcr,
-  vcr_cassette_library_dir: "test/fixture/vcr_cassettes",
-  filter_sensitive_data: [
-    [pattern: "token [^\"]+", placeholder: "token yourtokencomeshere"]
-  ]
+if Mix.env() == :test do
+  config :exvcr,
+    vcr_cassette_library_dir: "test/fixture/vcr_cassettes",
+    filter_sensitive_data: [
+      [pattern: "token [^\"]+", placeholder: "token yourtokencomeshere"]
+    ]
+end
