dc2ac9130c4a84e1be5a9e0a71fcda33f5b3d67a
Eduardo <eduardo@sendle.com>
2020-10-13 17:18:44 +1300

    Bump version 2.2.0

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Tentacat.Mixfile do
   def project do
     [
       app: :tentacat,
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.9",
       name: "Tentacat",
       description: @description,
